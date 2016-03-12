6e32fd5903667b0b94294bc02bc0b89c53a1ef20
Kirill Zaitsev <kirik910@gmail.com>
2016-03-13 00:09:45 +0300

    List of rubies

--- grvm.go
+++ grvm.go
@@ -46,6 +46,17 @@ func main() {
 	}
 	app.Commands = []cli.Command{
 		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "Show list installed rubies",
+			Action:  list,
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name: "known, k",
+				},
+			},
+		},
+		{
 			Name:    "env",
 			Aliases: []string{"e"},
 			Usage:   "Show env for ruby version",
@@ -180,6 +191,48 @@ func rebuildPaths(path, home string) string {
 
 	return strings.Join(newPaths, ":")
 
+}
+
+func list(c *cli.Context) {
+	var installedRubies []string
+	var knownRubies []string
+
+	db, err := getDB()
+	if err != nil {
+		Print(c, "Cannot open database file:", dbPath)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin(false)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer tx.Rollback()
+
+	b := tx.Bucket([]byte("rubies"))
+	cursor := b.Cursor()
+
+	for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
+		if v != nil && len(v) != 0 {
+			installedRubies = append(installedRubies, string(k))
+		} else {
+			knownRubies = append(knownRubies, string(k))
+		}
+	}
+
+	fmt.Println("installed rubies:")
+	for _, ruby := range installedRubies {
+		fmt.Println("  ", ruby)
+	}
+
+	if c.Bool("known") {
+		fmt.Println("known rubies:")
+		for _, ruby := range knownRubies {
+			fmt.Println("  ", ruby)
+		}
+	}
 }
 
 func doctor(c *cli.Context) {
