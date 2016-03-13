a58f84532c4f0cf0360c36d78634f69dd85f8743
Kirill Zaitsev <kirik910@gmail.com>
2016-03-13 21:37:22 +0300

    Move set to set.go

--- grvm.go
+++ grvm.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/boltdb/bolt"
 	"github.com/codegangsta/cli"
 )
 
@@ -90,56 +89,4 @@ func main() {
 	}
 
 	app.Run(os.Args)
-}
-
-func set(c *cli.Context) {
-	db, err := getDB()
-	if err != nil {
-		Print(c, "Cannot open database file:", dbPath)
-		os.Exit(1)
-	}
-	defer db.Close()
-
-	tx, err := db.Begin(true)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer tx.Rollback()
-
-	var b *bolt.Bucket
-	var e error
-	b, e = tx.CreateBucket([]byte("settings"))
-	if e == bolt.ErrBucketExists {
-		b = tx.Bucket([]byte("settings"))
-	} else if e != nil {
-		Print(c, "Cannot create bucket for settings")
-		os.Exit(1)
-	}
-
-	candidate := c.Args().Get(0)
-
-	if len(candidate) == 0 {
-		Print(c, "No version given")
-		os.Exit(1)
-	}
-
-	if err := checkCandidate(tx, candidate); err != nil {
-		Print(c, err.Error())
-		os.Exit(1)
-	}
-
-	if c.Bool("default") {
-		b.Put([]byte("default"), []byte(candidate))
-
-		if err := tx.Commit(); err != nil {
-			fmt.Println("Cannot save settings")
-			os.Exit(1)
-		}
-
-		printEnv(candidate)
-		Print(c, "Now,", candidate, "default ruby")
-	} else {
-		printEnv(candidate)
-	}
 }
---
+++ set.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func set(c *cli.Context) {
+	db, err := getDB()
+	if err != nil {
+		Print(c, "Cannot open database file:", dbPath)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin(true)
+	if err != nil {
+		Print(c, err.Error())
+		os.Exit(1)
+	}
+	defer tx.Rollback()
+
+	b, err := getBucket(tx, []byte("settings"))
+	if err != nil {
+		Print(c, err.Error())
+		os.Exit(1)
+	}
+
+	candidate := c.Args().Get(0)
+
+	if len(candidate) == 0 {
+		Print(c, "No version given")
+		os.Exit(1)
+	}
+
+	if err := checkCandidate(tx, candidate); err != nil {
+		Print(c, err.Error())
+		os.Exit(1)
+	}
+
+	if c.Bool("default") {
+		b.Put([]byte("default"), []byte(candidate))
+
+		if err := tx.Commit(); err != nil {
+			fmt.Println("Cannot save settings")
+			os.Exit(1)
+		}
+
+		printEnv(candidate)
+		Print(c, "Now,", candidate, "default ruby")
+	} else {
+		printEnv(candidate)
+	}
+}
